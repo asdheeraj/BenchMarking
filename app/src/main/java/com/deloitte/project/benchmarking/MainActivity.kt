442ffe79d26d64427e788533e58ac5a386368599
Andra Sai Sree Dheeraj <adheeraj@deloitte.com>
2020-11-24 11:53:48 +0530

    initial commit

---
+++ app/src/main/java/com/deloitte/project/benchmarking/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.deloitte.project.benchmarking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    //TODO: create a Navigation Component and make the app a Single Activity App
+}
